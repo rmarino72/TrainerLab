4b1bf724ba31e22055bc802aaf116c4a32939c74
Rosario Marino <rosariomarino72@gmail.com>
2023-05-22 11:41:25 +0200

    [DEV] Restored set pass page

---
+++ TrainerLab/web/SetPass.aspx.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Web.UI;
+
+namespace TrainerLab.web;
+
+public partial class SetPass : Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+}
